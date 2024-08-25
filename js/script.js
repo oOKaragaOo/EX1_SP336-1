0905af5760175a66a762449dda028205b4133dbf
Nattaphonng KONGNANG-OM <2210511101021@live4.utcc.ac.th>
2024-08-25 13:55:10 +0700

    badway

--- js/script.js
+++ js/script.js
@@ -1,24 +1,30 @@
 const canvas = document.getElementById('mycanvas');
 const ctx = canvas.getContext('2d');
 
-canvas.width = 800;
-canvas.height = 450;
+canvas.width = 400;
+canvas.height = 225;
 const image1 = new Image();
 
 image1.src = 'fig/apples.jpg';
 
     image1.addEventListener('load', function(){
         ctx.drawImage(image1, 0, 0, canvas.width, canvas.height);
-        const scannedImage = ctx.getImageData(0,0, canvas.width, 
-        canvas.height);
+        const scannedImage = ctx.getImageData(0,0, canvas.width, canvas.height);
         const scannedData = scannedImage.data; 
-    for (let i = 0; i < scannedData.length; i+=4){
-    //    scannedData[i] = 0; //Red
-        scannedData[i+1] = 2000; //Green
+
+        
+
+        RedInput.addEventListener('input', function(){
+            for (let i = 0; i < scannedData.length; i+=4){
+        
+        // scannedData[i] = 0; //Red
+    //    scannedData[i+1] = 0; //Green
     //    scannedData[i+2] = 0; //Blue
     //    scannedData[i+3] = 0; //Alpha
     }
-        console.log(scannedData)
+        });
+    
+        console.log(RedInput.value)
         scannedImage.data = scannedData;
         ctx.putImageData(scannedImage, 0, 0);
-});
+    });
