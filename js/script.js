ccc4b1d52744df525cdb8926c70929c691f32bc5
Nattaphonng KONGNANG-OM <2210511101021@live4.utcc.ac.th>
2024-08-25 20:51:04 +0700

    Add Alpha value and something comment for explain and done !

--- js/script.js
+++ js/script.js
@@ -1,33 +1,36 @@
 const canvas = document.getElementById('mycanvas');
 const ctx = canvas.getContext('2d');
-canvas.width = 400;
-canvas.height = 225;
+canvas.width = 800;
+canvas.height = 450;
 const image1 = new Image();
-var scannedData;
-image1.src = 'fig/apples.jpg';
+var originColours;
 
+//scannedImage for save base colours
+image1.src = 'fig/apples.jpg';
 image1.addEventListener('load', function(){
     ctx.drawImage(image1, 0, 0, canvas.width, canvas.height);
     const scannedImage = ctx.getImageData(0, 0, canvas.width, canvas.height);
-    scannedData = scannedImage.data;
+    originColours = scannedImage.data;
     modifyImageData();
 });
-
+//Time to RGB change ~
 function modifyImageData(){ 
     const scannedImage = ctx.getImageData(0, 0, canvas.width, canvas.height);
-    const scannedData2 = scannedImage.data;
+    const scannedData = scannedImage.data;
 
     const redValue = parseInt(RedInput.value);
     const greenValue = parseInt(GreenInput.value);
     const blueValue = parseInt(BlueInput.value); 
-
+    const AlphaValue = parseInt(AlphaInput.value); 
 
     for (let i = 0; i < scannedData.length; i += 4){
-        scannedData2[i] =  Math.min(255, scannedData[i] + redValue);
-        scannedData2[i+1] = Math.min(255, scannedData[i+1] + greenValue);
-        scannedData2[i+2] = Math.min(255, scannedData[i+2] + blueValue);
+        scannedData[i] =  Math.min(255, originColours[i] + redValue);
+        scannedData[i+1] = Math.min(255, originColours[i+1] + greenValue);
+        scannedData[i+2] = Math.min(255, originColours[i+2] + blueValue);
+        scannedData[i+3] = Math.min(255, originColours[i+3] + AlphaValue);
     }
-    
-    console.log(scannedData[1])
     ctx.putImageData(scannedImage, 0, 0);
 }
+
+
+// <!-- nattaphong KONGNANG-OM 2210511101021 -->
--- js/scriptRange.js
+++ js/scriptRange.js
@@ -1,20 +1,36 @@
+//Add for modifyIMG
 const RedInput = document.getElementById('RedInput');
 const GreenInput = document.getElementById('GreenInput');
 const BlueInput = document.getElementById('BlueInput');
+const AlphaInput = document.getElementById('AlphaInput');
 
+//Add for show value
 const RedValue = document.getElementById('RedValue');
 const GreenValue = document.getElementById('GreenValue');
 const BlueValue = document.getElementById('BlueValue');
+const AlphaValue = document.getElementById('AlphaValue');
+
+function resetRGB() {
+    RedInput.value = 0;
+    GreenInput.value = 0;
+    BlueInput.value = 0;
+    AlphaInput.Value = 0 ;
+    updateValues();
+}
 
 function updateValues() {
     RedValue.textContent = RedInput.value;
     GreenValue.textContent = GreenInput.value;
     BlueValue.textContent = BlueInput.value;
+    AlphaValue.textContent = AlphaInput.value;
     modifyImageData();
 }
 
-
-// Add event listeners to the sliders
+// Add event listeners to the Ranges RGB
 RedInput.addEventListener('input', updateValues);
 GreenInput.addEventListener('input', updateValues);
 BlueInput.addEventListener('input', updateValues);
+AlphaInput.addEventListener('input', updateValues);
+
+
+// <!-- nattaphong KONGNANG-OM 2210511101021 -->
